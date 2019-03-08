e9f069b485335c09ba52395147994e1dbe689e9c
alfredoPiris <47152905+alfredoPiris@users.noreply.github.com>
2019-03-08 13:56:09 +0100

    noFinalizado

--- speed/src/app/app.module.ts
+++ speed/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeContainerComponent } from './home-container/home-container.component';
+import { HomePresenterComponent } from './home-presenter/home-presenter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeContainerComponent,
+    HomePresenterComponent
   ],
   imports: [
     BrowserModule,
---
+++ speed/src/app/home-presenter/home-presenter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-presenter',
+  templateUrl: './home-presenter.component.html',
+  styleUrls: ['./home-presenter.component.css']
+})
+export class HomePresenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
